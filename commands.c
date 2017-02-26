2ad0e0e2df2a5820fdf3eaa77b8119c5c5a9f8a1
toogley <toogley@mailbox.org>
2017-02-26 21:24:14 +0100

    small refactoring
    * replace ESC_CHAR macro with function
    * replace CHECK_PAGER macro with function
    return 0 was added to prevent warning "control reaches end of non-void function
    [-Wreturn-type]"
    
    * added braces to if logic to improve readability

--- commands.c
+++ commands.c
@@ -1045,10 +1045,12 @@ int mutt_check_traditional_pgp (HEADER *h, int *redraw)
   else
   {
     for (i = 0; i < Context->vcount; i++)
-      if (Context->hdrs[Context->v2r[i]]->tagged &&
-	  !(Context->hdrs[Context->v2r[i]]->security & PGP_TRADITIONAL_CHECKED))
-	rv = _mutt_check_traditional_pgp (Context->hdrs[Context->v2r[i]], redraw)
-	  || rv;
+      if ((Context->hdrs[Context->v2r[i]]->tagged) && 
+          !(Context->hdrs[Context->v2r[i]]->security & PGP_TRADITIONAL_CHECKED))
+      {
+        rv = _mutt_check_traditional_pgp (Context->hdrs[Context->v2r[i]],redraw)
+                  || rv;
+      }
   }
   return rv;
 }
