fcbe9a8b2988430cd7422fa7bc30a430d6a17c0e
somini <somini@users.noreply.github.com>
2017-04-06 23:42:05 +0100

    Detect missing function mapping
    Move the missing function detection code into the `try_bind` function.
    Improve the error message to include the menu name.

--- keymap.c
+++ keymap.c
@@ -960,12 +960,14 @@ try_bind (char *key, int menu, char *func, const struct binding_t *bindings, BUF
 {
   int i;
 
-  for (i = 0; bindings[i].name; i++)
+  for (i = 0; bindings[i].name; i++) {
     if (mutt_strcmp (func, bindings[i].name) == 0)
     {
       return km_bindkey_err (key, menu, bindings[i].op, err);
     }
-  return -1;
+  }
+  snprintf (err->data, err->dsize, _("Function '%s' not available for menu '%s'"), func, mutt_getnamebyvalue(menu, Menus));
+  return -1; /* Couldn't find an existing function with this name */
 }
 
 const struct binding_t *km_get_table (int menu)
@@ -1047,7 +1049,7 @@ int mutt_parse_bind (BUFFER *buf, BUFFER *s, unsigned long data, BUFFER *err)
         r = try_bind (key, menu[i], buf->data, OpGeneric, err);
       }
       else {
-        r = -1; /* If you don't bind on generic, bind on the actual menu*/
+        r = -2; /* If you don't bind on generic, bind on the actual menu*/
       }
       if (r != 0)
       {
@@ -1055,9 +1057,6 @@ int mutt_parse_bind (BUFFER *buf, BUFFER *s, unsigned long data, BUFFER *err)
         if (bindings)
         {
           r = try_bind (key, menu[i], buf->data, bindings, err);
-          if (r == -1) {
-            snprintf (err->data, err->dsize, _("%s: no such function in map"), buf->data);
-          }
         }
       }
     }
